a0945d1e31cecc5d09ec14e2126f044f72291cdc
MrAymenDer <91115646+MrAymenDer@users.noreply.github.com>
2021-12-07 18:22:26 +0100

    skapa express

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
 const express = require ("express");
+let fs = require("fs");
 
 const app = express();
-
+app.use(express.static("publik"));
 const port = 3000;
+
+// när vi startar  o loggar in i localhost:3000 , då kommer vi till den sidan
 app.get("/", (req,res)=>{
     res.sendFile(__dirname + "/views/guestBook.html");
-});    //Hur man skapar en rout. Skickar meddelande till klienter
+    fs.readFile("views/guestBook.html", function(err, htmlData) {
+        fs.readFile("myGuestBook.txt", (err, textData) => {    
+            if (err) throw err; // avbryt exekveringen om fel
+            let textText = textData.toString();
+                if(textText){
+                let htmlText = htmlData.toString();
+                let output = htmlText.replace(/Finns_inget_data/, textText);
+                // skicka till klienten
+                res.send(output);
+        }});
+    })
+});   
+
+// hanterar POST-förfrågningar
+// OBS - behövs för att läsa data som skickas med POST
+app.use(express.urlencoded({extended: true}));
+
+//här får vi data från guestBook.html, som skickas genom <form action="/" metodhe="post"> 
+app.post("/", (req, res) => {
+            const myText ={
+                 namn : req.body.namn, 
+                 email : req.body.email,
+                 hemsida : req.body.hemsida,
+                 kommentar : req.body.kommentar,
+                 telefon : req.body.telefon,
+            } // skapar min konstant my text 
+    
+    const text = JSON.stringify(myText)
+    fs.appendFile("myGuestBook.txt", `${text}<br/>`, (err) => { // OBS - skapar filen om den inte redan finns, lägger annars till befintlig text
+        if(err) throw err;
+        fs.readFile("views/guestBook.html", function(err, htmlData) {
+            fs.readFile("myGuestBook.txt", (err, textData) => {    
+                if (err) throw err; // avbryt exekveringen om fel
+                    let htmlText = htmlData.toString();
+                    let textText = textData.toString();
+                
+                    let output = htmlText.replace(/Finns_inget_data/, textText);
+                    // skicka till klienten
+                    res.send(output);
+            });
+        })
+    });
+});
 
 
-// app.get("/about", about);// Hur man skickar filer till klienter
-// function about(req, res){
-//      res.sendFile(__dirname + "/views/about.html");
-// }
-
-// app.get("/*", badRequest); //Hur man tar hand om bad request. Ska alltid vara sist!
-// function badRequest(req, res){
-//     res.status(404).send("Not Found");
-// }
-
-
-app.listen(port , () => console.log("listening on port" + port));
+app.listen(port , () => console.log("listening on port: " + port));
